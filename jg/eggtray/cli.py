59db3aeec9a91d19b77fd49ea1371e6b801df9d7
Honza Javorek <mail@honzajavorek.cz>
2024-05-13 16:26:24 +0200

    logging and a few edits

--- jg/eggtray/cli.py
+++ jg/eggtray/cli.py
@@ -17,24 +17,26 @@ logger = logging.getLogger("jg.eggtray")
 @click.option("--github-api-key", envvar="GITHUB_API_KEY", help="GitHub API key.")
 def main(profiles_dir: Path, debug: bool, github_api_key: str | None = None):
     logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
-    profiles_paths = profiles_dir.glob("*.yml")
+    profiles_paths = list(profiles_dir.glob("*.yml"))
     if not profiles_paths:
         logger.error("No profiles found in the directory")
         raise click.Abort()
+    logger.info(f"Found {len(profiles_paths)} profiles")
     profiles = [load_profile(profile_path) for profile_path in profiles_paths]
     asyncio.run(check_profiles(profiles, github_api_key=github_api_key))
 
 
 def load_profile(profile_path: Path) -> dict:
     profile = parse(profile_path.read_text())
-    profile["username"] = profile_path.stem
-    profile["url"] = f"http://github.com/{profile_path.stem}"
+    username = profile_path.stem
+    profile["username"] = username
+    profile["url"] = f"https://github.com/{username}"
     return profile
 
 
 async def check_profiles(profiles: list[dict], github_api_key: str | None = None):
     for profile in profiles:
-        logger.info(profile["url"])
+        logger.info(f"Checking {profile['url']}")
         summary = await check_profile_url(
             profile["url"], raise_on_error=True, github_api_key=github_api_key
         )
